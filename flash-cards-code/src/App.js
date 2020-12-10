e9eeca1610673775fc3aa4be860fe1d66240c7ec
Rick Marshall <isben22@gmail.com>
2020-12-09 19:27:45 -0500

    title bar updates and connection to the db

--- flash-cards-code/src/App.js
+++ flash-cards-code/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import TitleBar from "./components/TitleBar/TitleBar.jsx";
+import Collection from "./components/collection/collection.jsx";
 
 function App() {
   return (
-    <TitleBar />
+  //  <div><TitleBar /></div> 
+   <div>
+    <Collection />
+    </div>
   );
 } 
 export default App;
--- flash-cards-code/src/components/collection/collection.jsx
+++ flash-cards-code/src/components/collection/collection.jsx
@@ -1,2 +1,34 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';
 
+
+
+class Collection extends React.Component {
+    state = {
+        collections: [],
+        loading: true
+    }
+
+// componentDidMount() {
+//     axios.get(`http://localhost:5000/api/collections`)  // need to change the connection string
+//     .then(res => {
+//         const collections = res.data;
+//         console.log(collections);
+//         this.setState({collections});
+//         this.setState({loading:false})
+//     })
+
+
+render() {
+    return (
+        // <ul>
+        // {this.state.collection.map}
+        // </ul>
+
+        //)
+    }
+}
+
+
+
+export default Collection
