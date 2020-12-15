b4d08fea10e5b33eaaba523abccc15410fd1b026
marcus-baumgardner <dwayne.jerski@gmail.comgit config --global user.name marcus-baumgardnergit config --global user.email dwayne.jerski@gmail.com>
2020-12-14 20:34:18 -0600

    got view definition buttion working properly. Fixed bug that it wouldn't flip card on next/previous/collecitons buttons

--- flash-cards-code/src/components/flashCardCreator/flashCardCreator.jsx
+++ flash-cards-code/src/components/flashCardCreator/flashCardCreator.jsx
@@ -20,7 +20,7 @@ function CreateFlashCard(props){
                 <div className=""></div>
                 <div>
                   <h1>{props.collection.cards[props.currentFlashCardNumber].word}</h1>
-                  <Button>View Definiton</Button>
+                  <Button onClick={()=> props.flip()}>View Definiton</Button>
                 </div>
                 <Button color="primary" onClick={()=> props.viewCollections()}>Collections</Button>
               </div>
@@ -34,29 +34,31 @@ function CreateFlashCard(props){
           </div>
         );
     } else if (props.front === false){
-      <div class="container-fluid"><h1>What makes you S.P.E.C.I.A.L?<span>(Click card to reveal perk!)</span></h1>
-            <div class="row">
-              <div class="col-sm-2" id="leftControl">
-                <Button color="primary" onClick={()=> props.previousFlashCard()}>
-                  Previous Flash Card
-                </Button>
+      return(
+        <div class="container-fluid"><h1>What makes you S.P.E.C.I.A.L?<span>(Click card to reveal perk!)</span></h1>
+          <div class="row">
+            <div class="col-sm-2" id="leftControl">
+              <Button color="primary" onClick={()=> props.previousFlashCard()}>
+                Previous Flash Card
+              </Button>
+            </div>
+            <div class="col-sm-6" id="collectionItem">
+              <div className=""></div>
+              <div>
+                <h1>{props.collection.cards[props.currentFlashCardNumber].definition}</h1>
+                <Button onClick={()=> props.flip()}>View Word</Button>
               </div>
-              <div class="col-sm-6" id="collectionItem">
-                <div className=""></div>
-                <div>
-                  <h1>{props.collection.cards[props.currentFlashCardNumber].word}</h1>
-                  <Button>View Definiton</Button>
-                </div>
-                <Button color="primary" onClick={()=> props.viewCollections()}>Collections</Button>
-              </div>
+              <Button color="primary" onClick={()=> props.viewCollections()}>Collections</Button>
+            </div>
 
-              <div class="col-sm-2" id="rightControl">
-                <Button color="primary" onClick={()=> props.nextFlashCard()}>
-                  Next Flash Card
-                </Button>
-              </div>
+            <div class="col-sm-2" id="rightControl">
+              <Button color="primary" onClick={()=> props.nextFlashCard()}>
+                Next Flash Card
+              </Button>
             </div>
           </div>
+        </div>
+      )
     }
     
 }
