819538970657866eb2dabf76ce3e369de6109fd3
marcus-baumgardner <dwayne.jerski@gmail.comgit config --global user.name marcus-baumgardnergit config --global user.email dwayne.jerski@gmail.com>
2020-12-15 19:13:09 -0600

    added code to editor.jsx to view table of entries, added buttons and fields for new submissionns, though they do nothing as of yet.

--- flash-cards-code/src/components/collection/collection.jsx
+++ flash-cards-code/src/components/collection/collection.jsx
@@ -6,20 +6,20 @@ import CardPlayer from '../card_player/card_player'
 
 function Collection(props) {
   return (
-    <div class="container-fluid"><h1>What makes you S.P.E.C.I.A.L?<span>(Click card to reveal perk!)</span></h1>
-      <div class="row">
-        <div class="col-sm-2" id="leftControl">
+    <div className="container-fluid"><h1>What makes you S.P.E.C.I.A.L?<span>(Click card to reveal perk!)</span></h1>
+      <div className="row">
+        <div className="col-sm-2" id="leftControl">
           <Button color="primary" onClick={() => props.previousCollection()}>
             Previous Collection
           </Button>{" "}
         </div>
-        <div class="col-sm-6" id="collectionItem">
+        <div className="col-sm-6" id="collectionItem">
           <div className="">{CardPlayer(props)}</div>
           <Button color="primary" onClick={() => props.viewFlashCards()}>View This Collection's Flash Cards</Button>
           <Button onClick={()=> props.viewEditor()}>Edit Collections</Button>
         </div>
 
-        <div class="col-sm-2" id="rightControl">
+        <div className="col-sm-2" id="rightControl">
           <Button color="primary" onClick={() => props.nextCollection()}>
             Next Collection
           </Button>
--- flash-cards-code/src/components/editor/editor.jsx
+++ flash-cards-code/src/components/editor/editor.jsx
@@ -1,15 +1,71 @@
 import React from 'react';
 import { Button } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './editor.css';
+import Form from 'react-bootstrap/Form';
+import Table from 'react-bootstrap/Table';
+
+
+function renderTableData(props) {
+    let currentCollection = [] 
+    currentCollection = props.collection;
+    console.log('editor collection: ', props.collection);
+    return(
+        currentCollection.cards.map((collection, index)=>{
+            const {id, word, definition} = collection
+            console.log('word: ', word)
+            console.log('definition: ', definition)
+            return(
+                <tr key={id}>
+                        <td>{word}</td>
+                        <td>{definition}</td>
+                        <td><Button>Remove</Button></td>    
+                </tr>
+                
+            )
+        })
+    )
+}
+function newFlashCardSubmit(){
+
+    return(
+        <tr>
+            <td><input type = "text" placeholder = "new word"/></td>
+            <td><input type = "text" placeholder = "new definition"/></td>
+            <td><Button>Submit</Button></td>
+        </tr>
+    )
+
+}
 
 
 function BuildEditor(props){
 
     return(
-        <div>
-            <h1>This is where the editor goes</h1>
+         <div>
+            <h1 id='title'>React Dynamic Table</h1>
+            <h3>NOW EDITING COLLECTION: {props.collection.title.toUpperCase()}</h3>
+            <div className="row">
+                <div className='col-md-2'><Button onClick={()=>props.previousCollection()} >Previous Collection</Button></div>
+                <div className='col-md-4'>
+                    <Table id='collection'> 
+                        <thead>
+                            <tr>
+                                <th>Word</th>
+                                <th>Definition</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {renderTableData(props)}
+                            {newFlashCardSubmit(props)}
+                        </tbody>
+                    </Table>
+                </div>
+                <div className = 'col-md-2'><Button onClick={()=>props.nextCollection()} >Next Collection</Button></div>
+            </div>
             <Button onClick={()=> props.viewCollections()}>View Collections</Button>
         </div>
-        
     );
 }
 
